ef6a8cca8d1652cdc64ed55535d61d379a205f4b
Vigneshwaran J <sjvignesh333@gmail.com>
2022-09-23 00:19:35 +0530

    SingleActivity RecyclerView list

--- app/src/main/java/com/example/socialmedialist/MainActivity.java
+++ app/src/main/java/com/example/socialmedialist/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.LinearLayout;
+
 
 public class MainActivity extends AppCompatActivity {
 
